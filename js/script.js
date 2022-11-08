9157890edda996ef692f4fddb69cf0359ff54a9e
ranopub <ranopub@hotmail.co.jp>
2022-11-08 14:43:31 +0900

    animate and canvas hover

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ const cvs = document.getElementById( "canvas" );
     // キャンバスのコンテキストを取得
 const context = cvs.getContext( "2d" );
 
-// context.beginPath();   // パスの初期化
-// context.fillStyle = "red";  // 塗りつぶしをセット
-// context.strokeStyle = "blue"; // 線色をセット
-// context.lineWidth = 3;   // 線幅をセット
-// context.rect(10 , 10 , 300 , 100); // 矩形の座標をパスにセット
-// context.fill();  // パスの情報をもとに塗りつぶし
-// context.stroke();  // パスの情報をもとに線を描画
+context.beginPath();   // パスの初期化
+context.fillStyle = "red";  // 塗りつぶしをセット
+context.strokeStyle = "blue"; // 線色をセット
+context.lineWidth = 3;   // 線幅をセット
+context.rect(0 , 0 , 300 , 300); // 矩形の座標をパスにセット
+context.fill();  // パスの情報をもとに塗りつぶし
+context.stroke();  // パスの情報をもとに線を描画
 
 // context.clearRect( 0 , 0 , cvs.clientWidth , cvs.clientHeight);
 
@@ -34,5 +34,27 @@ $(function(){
 		function(){
 			var i = $('.menu-item').index(this);
 			$('.menu-item').eq(i).css('width','30vw');
-		}	);
+		}	
+	);
+
+
+	$('.item-canvas').hover(
+		function(){
+
+			var i = $('.item-canvas').index(this);
+			var cnt = $('.item-canvas').eq(i).getContext( "2d" );
+			$('.item-canvas').eq(i).css('left','0%');
+			$('.item-canvas').eq(i).css('width','100%');
+			cnt.fillStyle = "red";
+			cnt.fill();
+			
+		},
+		function(){
+			var i = $('.item-canvas').index(this);
+			$('.item-canvas').eq(i).css('left','70%');
+			$('.item-canvas').eq(i).css('width','30%');
+			$('.item-canvas').eq(i).getContext( "2d" ).fillStyle = "blue";
+			$('.item-canvas').eq(i).getContext( "2d" ).fill();
+		}	
+	);
 });
