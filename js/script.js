e8b77efa3d947fd2af54bbbf26d9cb7029aca595
ranopub <ranopub0@gmail.com>
2022-12-05 14:47:12 +0900

    drawtile-checker-position

--- js/script.js
+++ js/script.js
@@ -2,12 +2,16 @@
 
 $(function(){
 
-	// $('body').append(
-	for (let i = 0; i < 256; i++) {
+	var baseColor = '#ffffff';
+	var mainColor = '#deffb3';
+
+	var drawTileQty = 512;
+
+	for (let i = 0; i < drawTileQty; i++) {
 		$('body').append($('<div class="draw-tile">'));
 	}
-	for (let i = 0; i < 256; i++) {
-		$('.draw-tile').eq(i).css('transform','translate(' + i + 'px, ' + i + 'px)');
+	for (let i = 0; i < drawTileQty; i++) {
+		$('.draw-tile').eq(i).css('transform','translate(' + ( (i*10)%160 + ((Math.floor(i/16)%4)*2) ) + 'px, ' + (Math.floor(i/16)*5) + 'px)');
 	}
 	
 	$('.tab-title').hover(
@@ -15,14 +19,14 @@ $(function(){
 		{
 			var i = $('.tab-title').index(this);
 			
-			$('.tab-title').eq(i).css('background-color','#ffffff');
+			$('.tab-title').eq(i).css('background-color',baseColor);
 			$('.tab-content').css('display','none');
 			$('.tab-title').eq(i).next('.tab-content').css('display','flex');
 		},
 		function()
 		{
 			var i = $('.tab-title').index(this);
-			$('.tab-title').eq(i).css('background-color','#bcff67');
+			$('.tab-title').eq(i).css('background-color',mainColor);
 
 		}	
 	);
