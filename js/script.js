b2fc6162ff0d5afd0acb09d7b66def59b3a6fbee
ranopub <ranopub0@gmail.com>
2023-01-19 14:49:26 +0900

    tile slide test

--- js/script.js
+++ js/script.js
@@ -4,15 +4,34 @@ $(function(){
 
 	var baseColor = '#ffffff';
 	var mainColor = '#deffb3';
-	var accentcolor = $('body').css('color');
+	var accentColor = $('body').css('color');
 
-	var drawTileQty = 512;
+	// ブラウザウィンドウ中心位置
+
+	// タイルDIV要素の数
+	var drawTileQty = 1024;
+	// X軸タイル数
+	var drawTileColumn = 16;
+	// タイルサイズXY
+	var drawTileSize = 16;
+	// タイルパターン周期
+	var drawTileCycle = 8;
+	// タイルX軸方向ずらし(px)
+	var drawTileXSlide = 2;
 
 	for (let i = 0; i < drawTileQty; i++) {
 		$('body').append($('<div class="draw-tile">'));
 	}
+
 	for (let i = 0; i < drawTileQty; i++) {
-		$('.draw-tile').eq(i).css('transform','translate(' + ( (i*10)%160 + ((Math.floor(i/16)%4)*2) ) + 'px, ' + (Math.floor(i/16)*5) + 'px)');
+		$('.draw-tile').eq(i).css('transform',
+			'translate('
+			+  (((i%drawTileColumn)*drawTileSize*2) + ( ((Math.floor(i/drawTileColumn))%drawTileCycle) ^ ((((Math.floor(i/drawTileColumn))%drawTileCycle)>>2)*7) *drawTileXSlide)) 
+			+  'px, '
+			+ (Math.floor(i/drawTileColumn)*drawTileSize) 
+			+ 'px)' );
+		//01234567>01234321 000 001 010 011 100 101 110 111 >000 001 010 011 100 011 010 001 x ^ x>>2<<2 001 111 1000 
+		
 	}
 	
 	$('.tab-title').hover(
